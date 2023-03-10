c25c139b73ce8ef2331402657201dd4b2b6fe373
Chiaghanam <leofazzy98@gmail.com>
2023-03-10 08:59:43 -0800

    my

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/*_puts: import*/
+
 void _puts(char *str);
 #include<stdio.h>
 /**
  * main - Entry point
- * _Puts - import function
+ *
  * _puts - import function
  * Return: Always 0 (Success)
  */
