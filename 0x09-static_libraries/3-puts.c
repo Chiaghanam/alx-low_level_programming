7f0c08f7de5abce9a03937fcf25dec433485eae1
Chiaghanam <leofazzy98@gmail.com>
2023-03-10 08:03:51 -0800

    libmy

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*_puts: import*/
+void _puts(char *str);
+#include<stdio.h>
+/**
+ * main - Entry point
+ * _Puts - import function
+ * _puts - import function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
